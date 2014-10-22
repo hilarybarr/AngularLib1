24a603f42297c6be7d3e393156c7fac6f75c8daf
Hilary <hilaryrosebarr@gmail.com>
2014-10-22 12:46:13 -0400

    .

--- app/libraryApp.js
+++ app/libraryApp.js
@@ -4,75 +4,110 @@
 
 var libraryApp = angular.module('libraryApp' , []);
 
-libraryApp.controller('libraryController',function($scope) {
+libraryApp.controller('libraryController',['$scope', '$http', function($scope, $http) {
+
+    $http.get('http://localhost:8000/app_dev.php/list').
+        success(function(data, status, headers, config) {
+            // this callback will be called asynchronously
+            // when the response is available
+            $scope.books=data;
+            console.log(data);
+        }).
+        error(function(data, status, headers, config) {
+           console.log("error")
+        });
 
 
-   //Initially populate books array
-   $scope.books=[
-        {
-            title: "Harry Potter",
-            author: "JK Rowling",
-            price: 5,
-            quantity: 2
-        },
-        {
-            title: "Jokebook",
-            author: "Me",
-            price: 3,
-            quantity: 1
-        }
-
-    ];
 
 
-    //Create book
-    $scope.createBook=function(formData){
-        $scope.books.push(
-            {
-                title: formData.title,
-                author: formData.author,
-                price: formData.price,
-                quantity: formData.quantity
-            })
-        $scope.formData={}; //clear form data
-    }
 
 
-    //Delete book
-    $scope.deleteBook=function($index){
-        $scope.books.splice($index, 1);
-    }
+ //   //Create book
+ //   $scope.createBook=function(formData){
+ //       $scope.books.push(
+ //           {
+ //               title: formData.title,
+ //               author: formData.author,
+ //               price: formData.price,
+ //               quantity: formData.quantity
+ //           })
+ //       $scope.formData={}; //clear form data
+ //   }
+ //
+ //
+ //   //Delete book
+ //   $scope.deleteBook=function($index){
+ //       $scope.books.splice($index, 1);
+ //   }
+ //
+ //
+ //   //Update book...This method isn't really necessary since aren't storing value..just wrote for practice
+ //   $scope.updateBook=function(book, $index) {
+ //       $scope.books[$index] = {
+ //           title: book.title,
+ //           author: book.author,
+ //           price: book.price,
+ //           quantity: book.quantity
+ //       }
+ //       return $scope.books;
+ //   }
+ //
+ //
+ //// $scope.searchField='';
+ //
+ //   //Total Quantity
+ //   $scope.totalQuantity=function(){
+ //       $scope.tquantity=0;
+ //
+ //       console.log($scope.searchField);
+ //
+ //       var searchQuery=$scope.searchField;
+ //
+ //           for (var i in $scope.books) {
+ //               $scope.tquantity += $scope.books[i]["quantity"];
+ //           }
+ //           return $scope.tquantity;
 
 
-    //Update book...This method isn't really necessary since aren't storing value..just wrote for practice
-    $scope.updateBook=function(book, $index) {
-        $scope.books[$index] = {
-            title: book.title,
-            author: book.author,
-            price: book.price,
-            quantity: book.quantity
-        }
-
-    }
 
 
-    //Total Quantity
-    $scope.totalQuantity=function(){
-        $scope.tquantity=0;
-        for (var i in $scope.books) {
-            $scope.tquantity += $scope.books[i]["quantity"];
-        }
-        return $scope.tquantity;
-    }
 
 
-    //Total Price
-    $scope.totalPrice=function(){
-        $scope.tprice=0;
-        for (var i in $scope.books) {
-            $scope.tprice += ($scope.books[i]["price"]* $scope.books[i]["quantity"]);
-        }
-        return $scope.tprice;
-    }
+    //WHY ISN'T THIS WORKING AND HOW CAN I WRITE IT BETTER? Right now just am hiding total price
+        // and total quantity if there's a searchField but I want totalPrice and totalQuantity
+        //to update based on searchField
 
-});
+        //if ($scope.searchField == undefined || $scope.searchField =='') {
+        //    for (var i in $scope.books) {
+        //        $scope.tquantity += $scope.books[i]["quantity"];
+        //    }
+        //    return $scope.tquantity;
+        //}else {
+        //    for (var i in $scope.books) {
+        //        for (var prop in $scope.books[i]) {
+        //            if ($scope.books[i][prop]==searchQuery {
+        //                $scope.tquantity += $scope.books[i]["quantity"];
+        //            }
+        //        }
+        //    }
+        //    return $scope.tquantity;
+        //
+        //}
+
+
+
+
+//
+//};
+//
+//
+//    //Total Price
+//    $scope.totalPrice=function(){
+//        $scope.tprice=0;
+//        for (var i in $scope.books) {
+//            $scope.tprice += ($scope.books[i]["price"]* $scope.books[i]["quantity"]);
+//        }
+//        return $scope.tprice;
+//    }
+
+}])
