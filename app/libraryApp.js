199d22df2cd127f9a6df8016c9ab7fe618b7fbf7
Hilary <hilaryrosebarr@gmail.com>
2014-10-23 14:22:12 -0400

    Step 4: Doctrine for list action complete

--- app/libraryApp.js
+++ app/libraryApp.js
@@ -6,35 +6,51 @@ var libraryApp = angular.module('libraryApp' , []);
 
 libraryApp.controller('libraryController',['$scope', '$http', function($scope, $http) {
 
-    $http.get('http://localhost:8000/app_dev.php/list').
-        success(function(data, status, headers, config) {
-            // this callback will be called asynchronously
-            // when the response is available
-            $scope.books=data;
-            console.log(data);
-        }).
-        error(function(data, status, headers, config) {
-           console.log("error")
-        });
+
+//Call Symfony show action to return JSON object with books
+//    $scope.refreshBooks = function () {
+        $http.get('http://localhost:8000/app_dev.php/show').
+            success(function (data, status, headers, config) {
+                // this callback will be called asynchronously
+                // when the response is available
+                $scope.books = data;
+                console.log(data);
+            }).
+            error(function (data, status, headers, config) {
+                console.log("error")
+            });
+    }
+//}
 
 
 
 
+    //   //Create book
+    $scope.createBook=function(formData){
+        $http.post('http://localhost:8000/app_dev.php/create').
+            success(function(data, status, headers, config) {
+                // this callback will be called asynchronously
+                // when the response is available
+                $scope.books=data;
+                console.log(data);
+            }).
+            error(function(data, status, headers, config) {
+                console.log("error")
+            });
 
 
- //   //Create book
- //   $scope.createBook=function(formData){
- //       $scope.books.push(
- //           {
- //               title: formData.title,
- //               author: formData.author,
- //               price: formData.price,
- //               quantity: formData.quantity
- //           })
- //       $scope.formData={}; //clear form data
- //   }
- //
- //
+
+        //$scope.books.push(
+        //    {
+        //        title: formData.title,
+        //        author: formData.author,
+        //        price: formData.price,
+        //        quantity: formData.quantity
+        //    })
+        $scope.formData={}; //clear form data
+    }
+
+
  //   //Delete book
  //   $scope.deleteBook=function($index){
  //       $scope.books.splice($index, 1);
