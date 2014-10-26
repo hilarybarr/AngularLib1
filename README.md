7e13a3a6de16486383b6b11ebfdb28f48c1f667a
Hilary Barr <hbarr@remedypartners.com>
2014-10-26 15:00:19 -0400

    Step 5 Fully Functioning CRUD App

--- README.md
+++ README.md
@@ -1,31 +1,12 @@
 #Hilary's Library Collection App
 
-- Modified package.json to include dependencies for the Express Node framework and Mongoose
-- Used Server.js to set up Express and connection to a MongoDB server (currently localhost, specified in config/database.js).
-- Instantiated ng-app in libraryApp.js
-- Set up model for books in models/book.js
-- Defined routes (app/routes.js) and service for working with $HTTP (app/libraryService.js)
-
 
 ##Installation
-- $ git clone https://github.com/hilarybarr/AngularLibrary.git
-- npm install
-- node server.js
-- (Run app from localhost: http://http://localhost/8080)
-
-##Questions:
-- How to get update to work? Should it be automatic because of ng-model or do I need Put methods (in MaybeUse.md)
-- totalQuantity (and totalPrice) methods
-- How can you keep Node server running?
-- Should app architecture be different?
-- When does Remedy use localStorage vs. Mongo?
-
-
-##To Do
-- Enable update
-- Make server responsive ($broadcast in routes/sockets)
-- Add Quantity and Price functions
-- Format currency
-- Host database on Modulus.io
+- $ git clone https://github.com/hilarybarr/AngularLib1.git
+- $ git clone https://github.com/hilarybarr/SymfonyLibrary1.git
+- In terminal:
+- Shell 1: $ mongod
+- Shell 2: $ cd SymfonyLibrary1 ; $ php app/console server:run
+- Open "http://localhost:63342/node-todo/app/index.html" in browser
 
 
--- app/libraryApp.js
+++ app/libraryApp.js
@@ -1,4 +1,3 @@
-
 var libraryApp = angular.module('libraryApp' , []);
 
 libraryApp.controller('libraryController',['$scope', '$http', function($scope, $http) {
@@ -17,7 +16,6 @@ libraryApp.controller('libraryController',['$scope', '$http', function($scope, $
     }
 
     $scope.load();
-
 
 
 //Create book
@@ -43,8 +41,47 @@ libraryApp.controller('libraryController',['$scope', '$http', function($scope, $
         });
         $scope.load();
 
+    };
+
+////Update book
+//PROBLEM: Only works as POST, not as PUT
+$scope.updateBook = function(book) {
+    console.log(book);
+    $http({
+        method: 'POST',
+        url: 'http://localhost:8000/app_dev.php/update/'+book.id,
+        data: book,
+        headers: {"Content-Type": "application/x-www-form-urlencoded"}  // set the headers so angular passing info as form data (not request payload)
+    });
+    $scope.load();
+
 };
 
+    $scope.searchField='';
+//Total Quantity
+   $scope.totalQuantity=function() {
+       $scope.tquantity = 0;
+
+       console.log($scope.searchField);
+
+       var searchQuery = $scope.searchField;
+
+       for (var i in $scope.books) {
+           $scope.tquantity += $scope.books[i]["quantity"];
+       }
+       return $scope.tquantity;
+
+   };
+
+
+ //Total Price
+    $scope.totalPrice=function(){
+        $scope.tprice=0;
+        for (var i in $scope.books) {
+            $scope.tprice += ($scope.books[i]["price"]* $scope.books[i]["quantity"]);
+        }
+        return $scope.tprice;
+    }
 
 
 }]);
@@ -54,103 +91,3 @@ libraryApp.controller('libraryController',['$scope', '$http', function($scope, $
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//   //Delete book
-//   $scope.deleteBook=function($index){
-//       $scope.books.splice($index, 1);
-//   }
-//
-//
-//   //Update book...This method isn't really necessary since aren't storing value..just wrote for practice
-//   $scope.updateBook=function(book, $index) {
-//       $scope.books[$index] = {
-//           title: book.title,
-//           author: book.author,
-//           price: book.price,
-//           quantity: book.quantity
-//       }
-//       return $scope.books;
-//   }
-//
-//
-//// $scope.searchField='';
-//
-//   //Total Quantity
-//   $scope.totalQuantity=function(){
-//       $scope.tquantity=0;
-//
-//       console.log($scope.searchField);
-//
-//       var searchQuery=$scope.searchField;
-//
-//           for (var i in $scope.books) {
-//               $scope.tquantity += $scope.books[i]["quantity"];
-//           }
-//           return $scope.tquantity;
-
-
-
-
-
-
-//WHY ISN'T THIS WORKING AND HOW CAN I WRITE IT BETTER? Right now just am hiding total price
-// and total quantity if there's a searchField but I want totalPrice and totalQuantity
-//to update based on searchField
-
-//if ($scope.searchField == undefined || $scope.searchField =='') {
-//    for (var i in $scope.books) {
-//        $scope.tquantity += $scope.books[i]["quantity"];
-//    }
-//    return $scope.tquantity;
-//}else {
-//    for (var i in $scope.books) {
-//        for (var prop in $scope.books[i]) {
-//            if ($scope.books[i][prop]==searchQuery {
-//                $scope.tquantity += $scope.books[i]["quantity"];
-//            }
-//        }
-//    }
-//    return $scope.tquantity;
-//
-//}
-
-
-
-
-//
-//};
-//
-//
-//    //Total Price
-//    $scope.totalPrice=function(){
-//        $scope.tprice=0;
-//        for (var i in $scope.books) {
-//            $scope.tprice += ($scope.books[i]["price"]* $scope.books[i]["quantity"]);
-//        }
-//        return $scope.tprice;
-//    }
